6c3ed4fbe07554e367e95a8bf38aee51c0c1a917
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2023-09-08 15:14:17 -0700

    Add projects section

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            External links featuring various projects I've worked on as well as various articles I've written and talks I've presented.
           </p>
         </div>
         <div className="container py-12">
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,28 @@
-const projectsData = [
+const ProjectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'd3.js Meetup Talk: A Pseudorandom Walkthrough of d3-random',
+    description: `I presented a walkthrough of the d3-random module of the popular JavaScript data-visualization library. This was a talk with the Bay Area d3 meetup group in 2018.`,
+    imgSrc: '/static/images/d3-random-talk.png',
+    href: 'https://www.youtube.com/watch?v=Fb1za8ZWhYw',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Hack Days at Mode',
+    description: `I wrote a piece for Mode's engineering blog on the company's hackathons.`,
+    imgSrc: '/static/images/mode-hack-days.png',
+    href: 'https://medium.com/mode-engineering/hack-days-at-mode-749a232901e2',
+  },
+  {
+    title: 'How to audit an AI model owned by someone else (part 1)',
+    description: `I was a coauthor on a tutorial detailing how to remotely audit AI models using OpenMined's library Syft.`,
+    imgSrc: '/static/images/openmined-audit-diagram.png',
+    href: 'https://blog.openmined.org/ai-audit-part-1/',
+  },
+  {
+    title: 'Announcing the 2023 xD Emerging Technology Fellows',
+    description: `This is a news piece on the xD website presenting my cohort of emerging technology fellows.`,
+    imgSrc: '/static/images/announcing-the-2023-xd-emerging-technology-fellows.png',
+    href: 'https://www.xd.gov/news/announcing-2023-xd-emerging-technology-fellows/',
   },
 ]
 
-export default projectsData
+export default ProjectsData
