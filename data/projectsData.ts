a45a011aea5016b81582839d1e8f5473c210cee6
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2023-09-08 15:21:42 -0700

    Reorder projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,15 +1,9 @@
 const ProjectsData = [
   {
-    title: 'd3.js Meetup Talk: A Pseudorandom Walkthrough of d3-random',
-    description: `I presented a walkthrough of the d3-random module of the popular JavaScript data-visualization library. This was a talk with the Bay Area d3 meetup group in 2018.`,
-    imgSrc: '/static/images/d3-random-talk.png',
-    href: 'https://www.youtube.com/watch?v=Fb1za8ZWhYw',
-  },
-  {
-    title: 'Hack Days at Mode',
-    description: `I wrote a piece for Mode's engineering blog on the company's hackathons.`,
-    imgSrc: '/static/images/mode-hack-days.png',
-    href: 'https://medium.com/mode-engineering/hack-days-at-mode-749a232901e2',
+    title: 'Announcing the 2023 xD Emerging Technology Fellows',
+    description: `This is a news piece on the xD website presenting my cohort of emerging technology fellows.`,
+    imgSrc: '/static/images/announcing-the-2023-xd-emerging-technology-fellows.png',
+    href: 'https://www.xd.gov/news/announcing-2023-xd-emerging-technology-fellows/',
   },
   {
     title: 'How to audit an AI model owned by someone else (part 1)',
@@ -18,10 +12,16 @@ const ProjectsData = [
     href: 'https://blog.openmined.org/ai-audit-part-1/',
   },
   {
-    title: 'Announcing the 2023 xD Emerging Technology Fellows',
-    description: `This is a news piece on the xD website presenting my cohort of emerging technology fellows.`,
-    imgSrc: '/static/images/announcing-the-2023-xd-emerging-technology-fellows.png',
-    href: 'https://www.xd.gov/news/announcing-2023-xd-emerging-technology-fellows/',
+    title: 'Hack Days at Mode',
+    description: `I wrote a piece for Mode's engineering blog on the company's hackathons.`,
+    imgSrc: '/static/images/mode-hack-days.png',
+    href: 'https://medium.com/mode-engineering/hack-days-at-mode-749a232901e2',
+  },
+  {
+    title: 'd3.js Meetup Talk: A Pseudorandom Walkthrough of d3-random',
+    description: `I presented a walkthrough of the d3-random module of the popular JavaScript data-visualization library. This was a talk with the Bay Area d3 meetup group in 2018.`,
+    imgSrc: '/static/images/d3-random-talk.png',
+    href: 'https://www.youtube.com/watch?v=Fb1za8ZWhYw',
   },
 ]
 
